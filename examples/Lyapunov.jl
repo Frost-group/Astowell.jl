7e0839b13258c04611f9414927c4d6ee4a5b03e8
IngvarsV <148284591+IngvarsV@users.noreply.github.com>
2026-08-10 11:36:34 +0100

    Bit more comments and logic.

--- examples/Lyapunov.jl
+++ examples/Lyapunov.jl
@@ -3,8 +3,8 @@ using LinearAlgebra
 using Astowell
 
 
-# Setup particle momenta and positions - currently for two particles, more is quite noisy
-r = [0.0 0.0; 0.0 0.0]
+# Setup particle momenta and positions - currently for two particles, more is too oscillatory to sample
+r = [-pi -pi; -pi -pi]
 k = [0.0 0.0; 1.0 0.0]
 
 # Allocate 1D line
@@ -14,18 +14,20 @@ data = zeros(N)
 
 for index in 1:N
 
-    # Shift the second particle away from the origin, linearly
-    r[2, 2] += pi / N
-    r[2, 1] += pi / N
+    # Shift the second particle away from the first, linearly and as much as possible
+    r[2, 2] += 2 * pi / N
+    r[2, 1] += 2 * pi / N
 
-    data[index] = real(det(A(r, k, a=1.0, N=2)))
+    # Calculate the difference between trajectories
+    data[index] = real(det(A(r, k, a=1.0, N=2)) - det(A(r, k, a=0.0, N=2)))
 
 end
 
-# Save 1D manifold
-writedlm("line.csv", data, ',')
+# Save final step bakcflow difference and resulting 1D manifold 
+write_ppm("examples/difference.ppm", renderimg(r, sampleimg(r, k, S=1024, a=1.0, N=2) - sampleimg(r, k, S=1024, a=0.0, N=2), S=1024, POW=1.0))
+writedlm("examples/manifold.csv", data, ',')
 
-# Calculate Lyapunov exponent
+# Calculate Lyapunov exponent - use long time limit, since we are far away from the regions with backflow impact
 exponent = 0.0
 
 for index in 2:(N-1)

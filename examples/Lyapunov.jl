fc7951a48af82b1c489b635c8e8e823889c4de05
IngvarsV <148284591+IngvarsV@users.noreply.github.com>
2026-08-17 22:42:32 +0100

    Adjust output for a first principles calculation.

--- examples/Lyapunov.jl
+++ examples/Lyapunov.jl
@@ -8,37 +8,58 @@ r = [-pi -pi; -pi -pi]
 k = [0.0 0.0; 1.0 0.0]
 
 # Allocate 1D line
-N = 10^6
+N = 10^3
 
-data = zeros(N)
+# Set range of whole box size
+η = 1e-3
 
-for index in 1:N
+# Allocate data
+data = zeros(N - 1, 2)
+
+# Avoid overflow
+r[2, 2] += η * 2 * pi / N
+r[2, 1] += η * 2 * pi / N
+
+for index in 1:(N-1)
+
+    Dₙ₋₁ = real(det(A(r, k, a=1.0, N=2)))
 
     # Shift the second particle away from the first, linearly and as much as possible
-    r[2, 2] += 2 * pi / N
-    r[2, 1] += 2 * pi / N
+    r[2, 2] += η * 2 * pi / N
+    r[2, 1] += η * 2 * pi / N
 
-    # Calculate the difference between trajectories
-    data[index] = real(det(A(r, k, a=1.0, N=2)) - det(A(r, k, a=0.0, N=2)))
+    # Calculate the distance between two particles
+    xₙ = sqrt((r[2, 2] - r[1, 2])^2 + (r[2, 1] - r[1, 1])^2) / (2 * pi)
+
+    Dₙ = real(det(A(r, k, a=1.0, N=2)))
+
+    # Shift again
+    r[2, 2] += η * 2 * pi / N
+    r[2, 1] += η * 2 * pi / N
+
+    Dₙ₊₁ = real(det(A(r, k, a=1.0, N=2)))
+
+    # Calculate derivative
+    derivative = (Dₙ₊₁ - Dₙ₋₁) / 2
+
+    # Adjust for a relative change
+    derivative /= Dₙ
+
+    # Adjust for the length of the box to be a unit
+    derivative *= N / η
+
+    # Save the logarithm of the derivative for exponent calculation
+    data[index, 2] = log(abs(derivative))
+
+    # Save coordinate value
+    data[index, 1] = xₙ
+
+    # Shift back positions
+    r[2, 2] -= η * 2 * pi / N
+    r[2, 1] -= η * 2 * pi / N
 
 end
 
-# Save final step bakcflow difference and resulting 1D manifold 
-write_ppm("examples/difference.ppm", renderimg(r, sampleimg(r, k, S=1024, a=1.0, N=2) - sampleimg(r, k, S=1024, a=0.0, N=2), S=1024, POW=1.0))
+# Save final step backflow and resulting 1D manifold 
+write_ppm("examples/backflow.ppm", renderimg(r, sampleimg(r, k, S=1024, a=1.0, N=2), S=1024, POW=1.0))
 writedlm("examples/manifold.csv", data, ',')
-
-# Calculate Lyapunov exponent - use long time limit, since we are far away from the regions with backflow impact
-exponent = 0.0
-
-for index in 2:(N-1)
-
-    # Calculate the relative derivative - I'm pretty sure we should normalizing here like I do
-    derivative = (data[index+1] - data[index-1]) / data[index] / 2
-
-    global exponent
-
-    exponent += log(abs(derivative))
-
-end
-
-println(exponent / N)
